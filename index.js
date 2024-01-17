bee2e855ab41f8985b963dafec288c3a526f76d4
umesh1702 <umeshdevineni1>
2024-01-17 15:13:24 +0530

    added comments

--- index.js
+++ index.js
@@ -2,27 +2,28 @@ const fs = require("fs");
 const {table, log} = require("console");
 const readline = require("readline");
 
+//to create interface for taking input
 const rl= readline.createInterface({
     input : process.stdin,
     output : process.stdout
 })
 
-
+//function to read the data from json files
  function inputjsonfile(filepath){
     const data =fs.readFileSync(filepath);
     return JSON.parse(data);
-
+   
  }
 
  filepaths=["clothing.json","electronics.json","books.json"];
 
  var completeData = [];
-
+//to copy data from json files and copy it to complete data array
  filepaths.forEach(filepath => {
     const loopData= inputjsonfile(filepath);
     completeData.push(loopData);
  });
- const completeDataString = JSON.stringify(completeData);
+// to take input from the user
 const showOptions=()=>{
     console.log("1. Books");
     console.log("2. clothings");
@@ -35,28 +36,39 @@ const showOptions=()=>{
     
 }
 
-
+//if condition to print the details of desired products
 function UserChoice(choice){
     if (choice === "1"){
 
        
         const booksInfo = completeData.find(type => type.category === 'books');
-        const booksInfoString = JSON.stringify(booksInfo);
-        console.table(booksInfo);
+        const productarray = booksInfo.products;
+        productarray.forEach(product=>
+            {
+             console.log("ID :", product.id, "\tName :", product.Name, "\tType :",product.Type,"\tAuthor",product.Author,"\tPrice",product.Price, "\tDiscount",product.Discount,"\tInventory",product.Inventory,"\tCurrency",product.currency);
+            });
+       
         rl.close();
     }
     else if (choice === "2"){
         const clothingsInfo = completeData.find(type => type.category === 'clothing');
-        const clothingsInfoString = JSON.stringify(clothingsInfo);
-        console.table(clothingsInfo);
+        const productarray = clothingsInfo.products;
+        productarray.forEach(product=>
+            {
+             console.log("ID :", product.id, "\tName :", product.Name, "\tType :",product.Type,"\tPrice",product.Price, "\tDiscount",product.Discount,"\tInventory",product.Inventory,"\tCurrency",product.currency);
+            });
+       
         rl.close()
 
         
     }
     else if (choice === "3") {
         const electronicsInfo = completeData.find(type => type.category === 'Electronics');
-        const electronicsInfoString = JSON.stringify(electronicsInfo);
-        console.table(electronicsInfo);
+        productarray.forEach(product=>
+            {
+             console.log("ID :", product.id, "\tName :", product.Name, "\tType :",product.Type,"\tPrice",product.Price, "\tDiscount",product.Discount,"\tBrand :",product.Brand,"\tInventory",product.Inventory,"\tCurrency",product.currency);
+            });
+       
         rl.close()
     }
     else {
@@ -65,7 +77,7 @@ function UserChoice(choice){
     }
 }
 
-showOptions();
+showOptions(); //to start with showOptions function
 
  
 
