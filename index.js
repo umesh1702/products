d1c96fe22646900d1c6fbddfc5f14ed4d891b16a
umesh1702 <umeshdevineni1>
2024-01-20 17:31:56 +0530

    added CRUD operations and proper indentation

--- index.js
+++ index.js
@@ -71,17 +71,20 @@ function UserChoice(product){
 
 
 showOptions(); //to start with showOptions function
-var dataPath=["","books.json","clothing.json","electronics.json"]
 
+var dataPath=["","books.json","clothing.json","electronics.json"]
 const col=["name","Type","Price","Discount","Inventory","Currency"];
 const options = ["","Add","Del","Modify"]
+const id =["id"]
+
 function userActions(){
     rl.question("Do you want to modify the data (FOR yes '1' and for No '2')  ",response=>{
         if(response === "1"){
     options.forEach((items,index) => { 
         if (index >= 1 && index <=3){
             console.log((index),(items));
-        }});
+        }
+    });
     console.log("4. exit");
     rl.question("choose your option: ",selectedOption=>{
     const choice = parseInt(selectedOption);
@@ -94,78 +97,76 @@ function userActions(){
         console.log("wrong option, Try again!!");
         rl.close();
     }else{
-    userIntrest(options[choice]);
+        userIntrest(options[choice]);
     };
 });
 }
-else{
-    console.log("You have not done any modifications")
-    rl.close();
-}
+    else{
+        console.log("You have not done any modifications ")
+        rl.close();
+    }
 })
 }
 
-const id =["id"]
 async function DataAdd(){
-    
-        const response = await askQuestion("To Which file do you want to Add?");
-        if(response >=1 && response <=3){
+    const response = await askQuestion("To Which file do you want to Add? ");
+    if(response >=1 && response <=3){
         const selectedProduct = completeData.find(type => type.category === productInfo[response]);
-            console.log("Fill the following");
-            const infObject = {};
-            for (const items of id){
-                infObject[items]=uid();  
-             }; 
-            for (const items of col){
-                const answer = await askQuestion(`Enter the value for ${items}`);
-                infObject[items]=answer  
-             };  
-            selectedProduct.products.push(infObject)            
-            var selectedProducts = JSON.stringify(selectedProduct,null,2);
-            fs.writeFileSync(dataPath[response],selectedProducts); 
-            console.log("your changes have been updated");
-            rl.close();
+        console.log("Fill the following");
+        const infObject = {};
+        for (const items of id){
+            infObject[items]=uid();  
+        }; 
+        for (const items of col){
+            const answer = await askQuestion(`Enter the value for ${items} `);
+            infObject[items]=answer  
+        };  
+        selectedProduct.products.push(infObject)            
+        var selectedProducts = JSON.stringify(selectedProduct,null,2);
+        fs.writeFileSync(dataPath[response],selectedProducts); 
+        console.log("your changes have been updated ");
+        rl.close();
             
     }  
-            else{
-                console.log("enter a valid option")
-                DataAdd();
-            }      
+    else{
+        console.log("enter a valid option")
+        DataAdd();
+        }      
 }
 
 async function DataModify(){
-        const response = await askQuestion("Which file do you want to modify?");
-        const selectedProduct = completeData.find(type => type.category === productInfo[response]);
-        const selection = await askQuestion("Write the id of the item to modify");
-        for (let i in selectedProduct.products){
-            console.log(selectedProduct.products[i].id,selectedProduct.products[i].Name)
-        const choice = await askQuestion("Which property do you need to modify")
-        }
-        for (let i in selectedProduct.products) {
-            if(selectedProduct.products[i].id === selection){
+    const response = await askQuestion("Which file do you want to modify? ");
+    const selectedProduct = completeData.find(type => type.category === productInfo[response]);
+    const selection = await askQuestion("Write the id of the item to modify ");
+    for (let i in selectedProduct.products){
+        console.log(selectedProduct.products[i].id,selectedProduct.products[i].Name)
+        const choice = await askQuestion("Which property do you need to modify ")
+    }
+    for (let i in selectedProduct.products) {
+        if(selectedProduct.products[i].id === selection){
                 
             }
         }
     }
 
 async function DataDelete(){
-    const response = await askQuestion("Which file do you want to Delete?");
+    const response = await askQuestion("Which file do you want to Delete? ");
     const selectedProduct = completeData.find(type => type.category === productInfo[response]);
     var selectedProducts = JSON.stringify(selectedProduct,null,2);
     for (let i in selectedProduct.products){
-    console.log(i,selectedProduct.products[i].id,selectedProduct.products[i].Name)
-}   
-    const selection = await askQuestion("write the index 0f the object to be deleted");
+        console.log(i,selectedProduct.products[i].id,selectedProduct.products[i].Name)
+    }   
+    const selection = await askQuestion("write the index 0f the object to be deleted ");
     const selected = parseInt(selection)
     if(response >=1 && response <=3){
-    for (let i in selectedProduct.products) {
-        if(selectedProduct.products[i].id === selectedProduct.products[selected].id){
-            selectedProduct.products.splice(i,1);
-            var selectedProducts = JSON.stringify(selectedProduct,null,2);
-            fs.writeFileSync(dataPath[response],selectedProducts);
-            rl.close();
+        for (let i in selectedProduct.products) {
+            if(selectedProduct.products[i].id === selectedProduct.products[selected].id){
+                selectedProduct.products.splice(i,1);
+                var selectedProducts = JSON.stringify(selectedProduct,null,2);
+                fs.writeFileSync(dataPath[response],selectedProducts);
+                rl.close();
+            }
         }
-    }
     }
     else{
         console.log("enter the right index");
@@ -188,15 +189,14 @@ function userIntrest (opinion){
         if (index >= 1 && index <=3){
             console.log((index),(items));
         }});
-     if (opinion === "Add"){
+    if (opinion === "Add"){
         DataAdd();
-        
-     }
-     else if(opinion === "Modify"){
+    }
+    else if(opinion === "Modify"){
         DataModify();
-     }
-     else if(opinion === "Del"){
+    }
+    else if(opinion === "Del"){
         DataDelete();
-     }
+    }
 }
 
