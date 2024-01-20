641afae0f429a8eebd75e61f6917e7fd76a74ef4
umesh1702 <umeshdevineni1>
2024-01-20 17:09:01 +0530

    added CRUD operations

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
 const readline = require("readline");
 const {Table, printTable} = require("console-table-printer");
+const { resolve } = require("path");
+const uid = require("uniqid");
+const inquirer = require("inquirer");
 
-filepaths=["clothing.json","electronics.json","books.json"];
+var filepaths=["clothing.json","electronics.json","books.json"];
 var completeData = [];
 
 //to create interface for taking input
@@ -24,7 +27,7 @@ const p =new Table({
 
 //to copy data from json files and copy it to complete data array
 filepaths.forEach(filepath => {
-    const loopData= inputjsonfile(filepath);
+    const loopData=inputjsonfile(filepath);
     completeData.push(loopData);
 });
 
@@ -33,10 +36,10 @@ productInfo= ["","books","clothing","Electronics"];
 const showOptions=()=>{
     productInfo.forEach((items,index) => { 
         if (index >= 1 && index <=3){
-        console.log((index),(items));
+            console.log((index),(items));
         }});
         console.log("4. exit");
-       rl.question("choose your option: ",selectedOption=>{
+        rl.question("choose your option: ",selectedOption=>{
         const choice = parseInt(selectedOption);
         if(choice == 4){
             console.log("Thank you for shopping");
@@ -52,7 +55,6 @@ const showOptions=()=>{
     });
 };
 
-
 //if condition to print the details of desired products
 function UserChoice(product){ 
     const selectedProduct = completeData.find(type => type.category === product);
@@ -62,15 +64,139 @@ function UserChoice(product){
         })
     });
     p.printTable();
-    rl.close();    
+        
+    userActions();
 };
 
 
 
 showOptions(); //to start with showOptions function
+var dataPath=["","books.json","clothing.json","electronics.json"]
 
- 
+const col=["name","Type","Price","Discount","Inventory","Currency"];
+const options = ["","Add","Del","Modify"]
+function userActions(){
+    rl.question("Do you want to modify the data (FOR yes '1' and for No '2')  ",response=>{
+        if(response === "1"){
+    options.forEach((items,index) => { 
+        if (index >= 1 && index <=3){
+            console.log((index),(items));
+        }});
+    console.log("4. exit");
+    rl.question("choose your option: ",selectedOption=>{
+    const choice = parseInt(selectedOption);
+    if(choice == 4){
+        console.log("All modifications have been saved");
+        rl.close();
+        return;
+    } 
+    if (choice > 4 || choice < 1){
+        console.log("wrong option, Try again!!");
+        rl.close();
+    }else{
+    userIntrest(options[choice]);
+    };
+});
+}
+else{
+    console.log("You have not done any modifications")
+    rl.close();
+}
+})
+}
 
- 
+const id =["id"]
+async function DataAdd(){
+    
+        const response = await askQuestion("To Which file do you want to Add?");
+        if(response >=1 && response <=3){
+        const selectedProduct = completeData.find(type => type.category === productInfo[response]);
+            console.log("Fill the following");
+            const infObject = {};
+            for (const items of id){
+                infObject[items]=uid();  
+             }; 
+            for (const items of col){
+                const answer = await askQuestion(`Enter the value for ${items}`);
+                infObject[items]=answer  
+             };  
+            selectedProduct.products.push(infObject)            
+            var selectedProducts = JSON.stringify(selectedProduct,null,2);
+            fs.writeFileSync(dataPath[response],selectedProducts); 
+            console.log("your changes have been updated");
+            rl.close();
+            
+    }  
+            else{
+                console.log("enter a valid option")
+                DataAdd();
+            }      
+}
 
- 
+async function DataModify(){
+        const response = await askQuestion("Which file do you want to modify?");
+        const selectedProduct = completeData.find(type => type.category === productInfo[response]);
+        const selection = await askQuestion("Write the id of the item to modify");
+        for (let i in selectedProduct.products){
+            console.log(selectedProduct.products[i].id,selectedProduct.products[i].Name)
+        const choice = await askQuestion("Which property do you need to modify")
+        }
+        for (let i in selectedProduct.products) {
+            if(selectedProduct.products[i].id === selection){
+                
+            }
+        }
+    }
+
+async function DataDelete(){
+    const response = await askQuestion("Which file do you want to Delete?");
+    const selectedProduct = completeData.find(type => type.category === productInfo[response]);
+    var selectedProducts = JSON.stringify(selectedProduct,null,2);
+    for (let i in selectedProduct.products){
+    console.log(i,selectedProduct.products[i].id,selectedProduct.products[i].Name)
+}   
+    const selection = await askQuestion("write the index 0f the object to be deleted");
+    const selected = parseInt(selection)
+    if(response >=1 && response <=3){
+    for (let i in selectedProduct.products) {
+        if(selectedProduct.products[i].id === selectedProduct.products[selected].id){
+            selectedProduct.products.splice(i,1);
+            var selectedProducts = JSON.stringify(selectedProduct,null,2);
+            fs.writeFileSync(dataPath[response],selectedProducts);
+            rl.close();
+        }
+    }
+    }
+    else{
+        console.log("enter the right index");
+        DataDelete();
+    }
+}
+
+
+function askQuestion(question){
+    return new Promise((resolve)=>{
+        rl.question(question,answer=>{
+            resolve(answer)
+        })
+    })
+}
+
+function userIntrest (opinion){
+    console.log("existing files")
+    dataPath.forEach((items,index) => { 
+        if (index >= 1 && index <=3){
+            console.log((index),(items));
+        }});
+     if (opinion === "Add"){
+        DataAdd();
+        
+     }
+     else if(opinion === "Modify"){
+        DataModify();
+     }
+     else if(opinion === "Del"){
+        DataDelete();
+     }
+}
+
