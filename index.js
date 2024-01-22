2d582f4e8e1d7fc331a236eec5a225354f579caf
umesh1702 <umeshdevineni1>
2024-01-22 16:54:21 +0530

    added prompt module

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
 const { resolve } = require("path");
-const lib = require("./lib.js")
+const lib = require("./lib.js");
 
 var completeData = lib.completeData
-const rl=lib.rl
-const option = ["View","Modify"]
-
-async function start(){
-    option.forEach((items,index)=>{
-    console.log((index),(items));
-})
-const choice= await askQuestion("Choose your option  ");
-const choiceInt = parseInt(choice);
-if (choiceInt === 0){
-    lib.showOptions();
-}
-else if(choiceInt === 1){
-    lib.userActions();
-}
-else {
-    console.log("choose correct option!!!!!!!");
-    start();
-}
-}
-
+const rl=lib.rl;
+const option = ["View","Modify"];
 
 //for promise
 function askQuestion(question){
     return new Promise((resolve)=>{
         rl.question(question,answer=>{
-            resolve(answer)
+            resolve(answer);
         })
     })
+}
+
+
+async function start(){
+    option.forEach((items,index)=>{
+    console.log((index),(items));
+    })
+    const choice= await askQuestion("Choose your option  ");
+    const choiceInt = parseInt(choice);
+
+    if (choiceInt === 0){
+        lib.showOptions();
+    }
+
+    else if(choiceInt === 1){
+        lib.userActions();
+    }
+
+    else {
+        console.log("choose correct option!!!!!!!");
+        start();
+    }
 }
 
 
--- lib.js
+++ lib.js
@@ -5,59 +5,68 @@ const { resolve } = require("path");
 const uid = require("uniqid");
 const prompt = require ("prompt");
 
+var completeData = [];
+var filepaths=["./assests/clothing.json","./assests/electronics.json","./assests/books.json"];
+productInfo= ["","books","clothing","Electronics"];
+var dataPath=["","books.json","clothing.json","electronics.json"];
+const col=["Name","Type","Price","Discount","Inventory","currency"];
+const options = ["","Add","Del","Modify"];
+const id =["id"];
 
-prompt.start();
 //to create interface for taking input
 const rl= readline.createInterface({
     input : process.stdin,
     output : process.stdout
 });
-var completeData = [];
-var filepaths=["./assests/clothing.json","./assests/electronics.json","./assests/books.json"];
+
+
 //function to read the data from json files
 function inputjsonfile(filepath){
     const data =fs.readFileSync(filepath);
     return JSON.parse(data);  
  };
+
+ //Loop to copy the details 
 filepaths.forEach(filepath => {
     const loopData=inputjsonfile(filepath);
     completeData.push(loopData);
 });
+
  // to create a table 
 const table =new Table({
     coloumns : [{Name : "ID"},{Name : "Name"},{Name : "Type"},{Name : "Price"},{Name : "Discount"},{Name : "Inventory"},{Name : "Currency"}]
 });
 
-//to copy data from json files and copy it to complete data array
-
-
-productInfo= ["","books","clothing","Electronics"];
-
 // To take input from the user
-const showOptions=()=>{
+async function  showOptions(){
     productInfo.forEach((items,index) => { 
         if (index >= 1 && index <=3){
             console.log((index),(items));
-        }});
-        console.log("4. exit");
-        rl.question("choose your option: ",selectedOption=>{
-        const choice = parseInt(selectedOption);
-        if(choice == 4){
-            console.log("Thank you!!!!!");
-            rl.close();
-            return;
-        } 
-        if (choice > 4 || choice < 1){
-            console.log("wrong option, Try again!!");
-            showOptions();
-        }else if (choice>=1 || choice<=3) {
-            UserChoice(productInfo[choice]);
         }
-        else {
-            console.log("enter correct option");
-            showOptions();
-       }
     });
+        console.log("4. exit");
+    const selectedOption = await askQuestion("choose your option: ")
+    const choice = parseInt(selectedOption);
+    if(choice == 4){
+        console.log("Thank you!!!!!");
+        rl.close();
+        return;
+        } 
+
+    if (choice > 4 || choice < 1){
+        console.log("wrong option, Try again!!");
+        showOptions();
+    }
+        
+    else if (choice>=1 || choice<=3) {
+        UserChoice(productInfo[choice]);
+    }
+
+    else {
+        console.log("enter correct option");
+        showOptions();
+    }
+
 };
 
 
@@ -72,10 +81,9 @@ function UserChoice(product){
     table.printTable();
     rl.close();
 };
-var dataPath=["","books.json","clothing.json","electronics.json"]
-const col=["Name","Type","Price","Discount","Inventory","currency"];
-const options = ["","Add","Del","Modify"]
-const id =["id"]
+
+
+
 
 //function to add the data
 async function DataAdd(){
@@ -84,14 +92,17 @@ async function DataAdd(){
         const selectedProduct = completeData.find(type => type.category === productInfo[response]);
         console.log("Fill the following");
         const infObject = {};
+
         for (const items of id){
             infObject[items]=uid();  
         }; 
+
         for (const items of col){
             const answer = await askQuestion(`Enter the value for ${items} `);
-            infObject[items]=answer  
+            infObject[items]=answer;  
         };  
-        selectedProduct.products.push(infObject)            
+
+        selectedProduct.products.push(infObject);           
         var selectedProducts = JSON.stringify(selectedProduct,null,2);
         fs.writeFileSync(`./assests/${dataPath[response]}`,selectedProducts); 
         console.log("your changes have been updated ");
@@ -99,33 +110,50 @@ async function DataAdd(){
             
     }  
     else{
-        console.log("enter a valid option")
+        console.log("enter a valid option");
         DataAdd();
         }      
 }
+
+
 //Function to modify the data
 async function DataModify(){
     const response = await askQuestion("Which file do you want to modify? ");
     const selectedProduct = completeData.find(type => type.category === productInfo[response]);
+
     for (let i in selectedProduct.products){
         console.log(i,selectedProduct.products[i].id,selectedProduct.products[i].Name)
-    }   
+    }  
+
     const selection = await askQuestion("write the index 0f the object to be modified ");
-    const selected = parseInt(selection)
-   col.forEach((item,index)=>{
-    console.log(index,item);
-   })
+    const selected = parseInt(selection);
+
+    col.forEach((item,index)=>{
+        console.log(index,item);
+    })
+
+    prompt.start();
     const choice = await askQuestion("Which property do you need to modify (NOTE: you cannot change id) ")
     const choiceInt = parseInt(choice);
-    console.log(col[choiceInt])
+    console.log(col[choiceInt]);
 
-    const changedProperty = await askQuestion(`Type the change `);
-    selectedProduct.products[selected][col[choice]]= changedProperty;          
+    const changedProperty = await prompt.get([
+        {
+        name : `${col[choiceInt]}`,
+        description : 'enter a value',
+        type : "string",
+        default : `${selectedProduct.products[selected][col[choice]]}`,
+        required : false
+        }
+    ]) 
+
+    const changedPropertyvalue = changedProperty[col[choice]];
+    selectedProduct.products[selected][col[choice]]= changedPropertyvalue;          
     var selectedProducts = JSON.stringify(selectedProduct,null,2);
     fs.writeFileSync(`./assests/${dataPath[response]}`,selectedProducts); 
     console.log("your changes have been updated ");
     rl.close();
-        }
+}
     
 
 //function to delete the data 
@@ -133,11 +161,14 @@ async function DataDelete(){
     const response = await askQuestion("Which file do you want to Delete? ");
     const selectedProduct = completeData.find(type => type.category === productInfo[response]);
     var selectedProducts = JSON.stringify(selectedProduct,null,2);
+
     for (let i in selectedProduct.products){
-        console.log(i,selectedProduct.products[i].id,selectedProduct.products[i].Name)
-    }   
+        console.log(i,selectedProduct.products[i].id,selectedProduct.products[i].Name);
+    }
+
     const selection = await askQuestion("write the index 0f the object to be deleted ");
     const selected = parseInt(selection)
+
     if(response >=1 && response <=3){
         for (let i in selectedProduct.products) {
             if(selectedProduct.products[i].id === selectedProduct.products[selected].id){
@@ -148,17 +179,20 @@ async function DataDelete(){
             }
         }
     }
+
     else{
         console.log("enter the right index");
         DataDelete();
     }
+
 }
 
 
+// for promise
 function askQuestion(question){
     return new Promise((resolve)=>{
         rl.question(question,answer=>{
-            resolve(answer)
+            resolve(answer);
         })
     })
 }
@@ -166,38 +200,40 @@ function askQuestion(question){
 
 //function to check whether user need to modify or not
 async function userActions(){
-    const response = await askQuestion("Do you want to modify the data (FOR yes '1' and for No '2')  ")
-        if(response === "1"){
-    options.forEach((items,index) => { 
-        if (index >= 1 && index <=3){
-            console.log((index),(items));
-        }
-    });
-    console.log("4. exit");
-    const selectedOption= await askQuestion("choose your option: ")
-    const choice = parseInt(selectedOption);
-    if(choice == 4){
-        console.log("Thank you!!! ");
-        rl.close();
-        return;
-    } 
-    else if (choice > 4 || choice < 1){
-        console.log("wrong option, Try again!!");
-        userActions();
-    }
-    
-    else if (choice>=1 || choice<=3) {
-        console.log(choice)
-        userIntrest(options[choice]);
-    }
-    else {
-        console.log("enter correct option");
-        userActions();
-   }
+    const response = await askQuestion("Do you want to modify the data (FOR yes '1' and for No '2')  ");
+    if(response === "1"){
+        options.forEach((items,index) => { 
+            if (index >= 1 && index <=3){
+                console.log((index),(items));
+            }
+        });
+        console.log("4. exit");
+        const selectedOption= await askQuestion("choose your option: ");
+        const choice = parseInt(selectedOption);
+        if(choice == 4){
+            console.log("Thank you!!! ");
+            rl.close();
+            return;
+        } 
 
-}
+        else if (choice > 4 || choice < 1){
+            console.log("wrong option, Try again!!");
+            userActions();
+        }
+        
+        else if (choice>=1 || choice<=3) {
+            console.log(choice);
+            userIntrest(options[choice]);
+        }
+
+        else {
+            console.log("enter correct option");
+            userActions();
+        }
+
+    }
     else if (response === "2"){
-        console.log("You have not done any modifications ")
+        console.log("You have not done any modifications ");
         rl.close();
     }
     else {
@@ -209,15 +245,14 @@ async function userActions(){
 
 
 
-
-
 //function to check what user needs
 function userIntrest (opinion){
-    console.log("existing files")
+    console.log("existing files");
     dataPath.forEach((items,index) => { 
         if (index >= 1 && index <=3){
             console.log((index),(items));
-        }});
+        }
+    });
     if (opinion === "Add"){
         DataAdd();
     }
@@ -228,4 +263,14 @@ function userIntrest (opinion){
         DataDelete();
     }
 }
-module.exports={rl,productInfo,completeData,table,showOptions,DataAdd,DataDelete,DataModify,userActions}
+
+
+module.exports={rl,productInfo,completeData,table,showOptions,DataAdd,DataDelete,DataModify,userActions};
+
+
+
+
+
+
+
+
